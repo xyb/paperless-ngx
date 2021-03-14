2bd2f48cb3d07c24d5ba28e962a3dee84f41b836
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2021-03-13 21:00:53 -0800

    Basic button for initiating split/merge from bulk editor

--- src-ui/src/app/components/document-list/document-list.component.ts
+++ src-ui/src/app/components/document-list/document-list.component.ts
@@ -14,6 +14,7 @@ import { SavedViewService } from 'src/app/services/rest/saved-view.service';
 import { ToastService } from 'src/app/services/toast.service';
 import { FilterEditorComponent } from './filter-editor/filter-editor.component';
 import { SaveViewConfigDialogComponent } from './save-view-config-dialog/save-view-config-dialog.component';
+import { SplitMergeService } from 'src/app/services/split-merge.service';
 
 @Component({
   selector: 'app-document-list',
@@ -29,7 +30,8 @@ export class DocumentListComponent implements OnInit, OnDestroy {
     private router: Router,
     private toastService: ToastService,
     private modalService: NgbModal,
-    private consumerStatusService: ConsumerStatusService
+    private consumerStatusService: ConsumerStatusService,
+    private splitMergeService: SplitMergeService
   ) { }
 
   @ViewChild("filterEditor")
@@ -175,5 +177,10 @@ export class DocumentListComponent implements OnInit, OnDestroy {
 
   trackByDocumentId(index, item: PaperlessDocument) {
     return item.id
+  }
+
+  addToSplitMerge() {
+    this.splitMergeService.addDocuments(this.list.selectedDocuments)
+    this.router.navigate(["split_merge"])
   }
 }
